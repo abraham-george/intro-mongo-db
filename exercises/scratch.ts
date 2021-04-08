3272e094b688a14b8154c3b75e3a009c73730735
abraham-george <abraham.george.j@gmail.com>
2021-04-08 16:22:50 -0500

    Initial Commit - uploading notes and package-lock.json file

---
+++ exercises/scratch.ts
@@ -0,0 +1,13 @@
+/**
+ * 
+ * This is basically my notes section where I will be adding keywords and notes during my learning process.
+ * 
+ * -    use mongod to start the mongo db service on your machine after installation
+ * -    use mongo to get into the interactive mongo shell
+ * -    use <some_db_name> creates a db and switches to it if it doesnt exist
+ * -    db.help() -> gives you helper commands in the shell
+ * -    collection in mongo is similar to table in SQL
+ * -    db.createCollection('<collection_name>') -> creates a collection
+ * -    
+ * 
+ */
