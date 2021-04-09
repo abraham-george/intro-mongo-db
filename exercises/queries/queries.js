87fa86aac038eb8f10598705e98a13262960c4be
abraham-george <abraham.george.j@gmail.com>
2021-04-09 11:16:57 -0500

    Completed exercises for queries.

--- exercises/queries/queries.js
+++ exercises/queries/queries.js
@@ -1,28 +1,23 @@
 const Post = require('./post')
 
-const postByTitle = (title) => {
+const postByTitle = (title) => Post.findOne({title: title}).exec()
 
-}
-
-const postsForAuthor = (authorId) => {
-
-}
-
-const fullPostById = (id) => {
+const postsForAuthor = (authorId) => Post.find({author: authorId}).exec()
   
-}
+const fullPostById = (id) => Post.findById(id).populate('author').exec()
 
-const allPostsSlim = (fieldsToSelect) => {
-  
-}
+const allPostsSlim = (fieldsToSelect) => Post.find({})
+.select(fieldsToSelect)
 
-const postByContentLength = (maxContentLength, minContentLength) => {
-  
-}
+const postByContentLength = (maxContentLength, minContentLength) =>
+  Post.find({
+    contentLength: { $gt: minContentLength, $lt: maxContentLength }
+  }).exec()
 
-const addSimilarPosts = (postId, similarPosts) => {
-  
-}
+const addSimilarPosts = (postId, similarPosts) =>
+  Post.findByIdAndUpdate(postId, {
+    $push: { similarPosts: { $each: similarPosts } },
+  }, {new: true}).exec();
 
 module.exports = {
   postByTitle,
